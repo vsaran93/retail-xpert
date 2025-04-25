b5008af18b6362baac4b819fb1442e45ac370c4e
vsaran93 <vishnusaran37@gmail.com>
2025-04-26 00:49:39 +0100

    login btn added to right corner in nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import {
 } from "@/components/ui/navigation-menu"
 import './globals.css'
 import { Button } from "@/components/ui/button"
-import { LayoutDashboard } from "lucide-react"
+import { LayoutDashboard, UserRound } from "lucide-react"
 
 
 function Layout({ children }: { children: React.ReactNode }) {
@@ -24,28 +24,27 @@ function Layout({ children }: { children: React.ReactNode }) {
         <body>
         <SidebarProvider defaultOpen={false} open={openSiderBar}>
           <div className="h-screen flex flex-col w-full">
-            <header className="bg-blue-500 p-2" style={{ paddingLeft: openSiderBar ? '16rem' : '3rem' }}>   
-              <NavigationMenu className='w-full'>
-                <NavigationMenuList className="flex items-center justify-between w-full">
-                <div className='flex items-center'>
-                  <NavigationMenuItem>
-                      <Button onClick={toggleSideBar} variant="ghost" className="text-white">
-                          <LayoutDashboard />
-                      </Button>
-                    </NavigationMenuItem>
-                  <NavigationMenuItem className="text-white ml-4">
-                    Retail-Xpert
-                  </NavigationMenuItem>
-                </div>
-                <div className='flex items-center'>
-                  <NavigationMenuItem className="text-white ml-4">
-                    Login
-                  </NavigationMenuItem>
-                </div>
-                  
-                </NavigationMenuList>
-                
-                </NavigationMenu>
+            <header className="bg-blue-500 p-2" style={{ paddingLeft: openSiderBar ? '16rem' : '3rem' }}> 
+              <div className='flex justify-between'>
+                  <div className='flex items-center'>
+                    <NavigationMenu>
+                      <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <Button onClick={toggleSideBar} variant="ghost" className="text-white">
+                                <LayoutDashboard />
+                            </Button>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem className="text-white ml-4">
+                          Retail-Xpert
+                        </NavigationMenuItem>
+                      </NavigationMenuList>
+                    </NavigationMenu>
+                  </div>
+                  <div className='flex items-center'>
+                    <Button className="text-white" variant="ghost"><UserRound />Login</Button>
+                  </div>
+              </div>  
+              
             </header>
             <div className="flex flex-1">
               <div className={`${openSiderBar ? 'w-64' : 'w-3'} transition-all duration-300 overflow-auto`}>
